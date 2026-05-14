9f0272e790e01f1425d9b6734c1452783f43a44f
Kishore M <142767995+kishore0013667@users.noreply.github.com>
2026-05-14 20:24:29 +0530

    Update portfolio website styling and scripts

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+console.log("JavaScript Connected");
+
 const textArray = [
     "Mern Stack Developer",
     "Frontend Developer",
@@ -21,3 +24,29 @@ function changeText(){
 setInterval(changeText, 2000);
 
 changeText();
+
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit" , function(event){
+
+    event.preventDefault();
+
+    alert("Message Sent Successfully!");
+    
+});
+
+const themeButton = document.getElementById("theme-toggle");
+
+themeButton.addEventListener("click",function(){
+
+    document.body.classList.toggle("light-mode");
+
+    if(document.body.classList.contains("light-mode")){
+
+        themeButton.innerText = "☀️";
+    }
+    else{
+        themeButton.innerText = "🌙";
+}
+
+});
